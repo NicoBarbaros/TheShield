a9f87b2850db7593023c7e65946bc147dac68fad
unknown <NicoBarbaros>
2015-05-14 01:06:51 +0200

    SplashScreen

---
+++ Assets/Scripts/SplashScreenLoader.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenLoader : MonoBehaviour {
+
+	public float delayTime = 5f;
+	public bool done = false;
+
+	private float timer;
+	// Use this for initialization
+	void Start () 
+	{
+	  	timer = delayTime;
+		StartCoroutine ("SomeFunction");
+	}
+
+	void Update()
+	{
+		Debug.Log (timer);
+		if(timer > 0)
+		{
+			timer -= Time.deltaTime;
+			return;
+		}
+
+		if(done)
+		{
+			Application.LoadLevel(1);
+		}
+	}
+
+	IEnumerator SomeFunction()
+	{
+		yield return null;
+		done = true;
+	}
+	
+
+}
